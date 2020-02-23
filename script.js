938e6244c4e7a9e99658e8846b896a4305a74098
Jeremy N <no-reply>
2020-02-23 16:36:19 -0700

    re made code again and got the btn to generate one random letter in the box

--- script.js
+++ script.js
@@ -7,40 +7,46 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
-}
+};
 
-var char ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var num = 123456789;
-var symbol = "!@#$%^&*()";
-var passLength = prompt("How many character would you like to use? min:8 max:128");
 
-confirm("Would you like to use Capitalization?");
-confirm("Would you like tl use Lowercase?");
-confirm("would you like to use Symbols?");
-confirm("Would you like to use Numbers?");
+// var pwd = "";
+// var character ="123456789!@#$%^&*()abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// var upper =;
+// var num = ;
+// var symbol = "";
+// var passLength = prompt("How many character would you like to use? min:8 max:128");
+// prompt user password criteria
+// confirm("Would you like to use Capitalization?");
+// confirm("Would you like tl use Lowercase?");
+// confirm("would you like to use Symbols?");
+// confirm("Would you like to use Numbers?");
 
 // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", writePassword, function(e){
-  // passLength.value;
-
-  symbol.value;
-  num.value;
-  password.value = generatedPassword(passLength.value, char)
-});
-
-
-
-function generatePassword(passLength, char){
-    var pwd = " ";
+generateBtn.addEventListener("click", writePassword);
   
 
-    for(var i = 0; i < passLength; i++){
-      pwd += char.charAt(Math.floor(Math.random() * char.length));
-      console.log(pwd);
-    }
-    return pwd;
+  // symbol.value;
+  // num.value;
+//   password.value = generatedPassword(passLength.value, char)
+// });
+
+
+
+function generatePassword(){
+  
+  var character ="123456789!@#$%^&*()abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var password = "";
+  var passLength = prompt("How many character would you like to use? min:8 max:128");
+ 
     
+      for(var i = 1; i < passLength; i++){
+      password =  character.charAt(Math.floor(Math.random() * Math.floor(character.length -1)));
+      
+    };
+  return password;
+  
 };
 
 
@@ -54,7 +60,6 @@ function generatePassword(passLength, char){
   // symbol: randomSymbol,
   // number: randomNumber,
 // };
-
 
 
 
@@ -77,4 +82,11 @@ function generatePassword(passLength, char){
 //   return String.fromCharCode(Math.floor(Math.random()* 9) + 49 );
 // }
 
+// if(passLength >= 8){
+//   upper.randomUpper();
+//   lower.randomLower();
+//   symbol.randomSymbol();
+//   num.randomNumber();
+// };
 
+// console.log( upper + lower + num + symbol * passLength)
